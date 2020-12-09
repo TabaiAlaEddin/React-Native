25048fc8f802abd75d15463242ab1b4b94330cfb
unknown <alaeddin.tabai@esprit.tn>
2020-12-09 21:53:20 +0100

    first commit

--- App.js
+++ App.js
@@ -3,9 +3,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Ahlaa skon</Text>
+      <Text>Ahlaa skon</Text>
+      <Text>Ahlaa skon</Text>
       <StatusBar style="auto" />
     </View>
   );
